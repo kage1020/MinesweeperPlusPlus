a9147abf699773326e1f34ab1dcf75216ab19fa7
kage1020 <73168765+kage1020@users.noreply.github.com>
2022-11-22 12:57:58 +0900

    💄 Add underline to link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -345,15 +345,23 @@ export default function Home() {
           href='https://github.com/kage1020'
           target='_blank'
           rel='noopener noreferrer'
-          className='p-2'
+          className='p-2 underline'
         >
           &copy; kage1020
+        </a>/
+        <a
+          href='https://github.com/kage1020/MinesweeperPlusPlus'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='p-2 underline'
+        >
+          MinesweeperPlusPlus
         </a>
         <a
           href='https://docs.google.com/forms/d/e/1FAIpQLSc_t3XtTd7dpoIcgaWoMOemFguE_BGj2px5sQ77ww0HkhKqYg/viewform?usp=pp_url&entry.1199509574=%E7%89%B9%E3%81%AB%E3%81%AA%E3%81%97'
           target='_blank'
           rel='noopener noreferrer'
-          className='p-2'
+          className='p-2 underline'
         >
           フィードバック
         </a>
